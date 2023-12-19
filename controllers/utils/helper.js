32325aa2f0120fd76639bf56fd632e971a3b4b89
Shivam Uttam <102919037+Fadedrifleman@users.noreply.github.com>
2023-12-19 12:52:22 +0530

    Delete controllers/utils directory

--- controllers/utils/helper.js
+++
@@ -1,9 +0,0 @@
-function calculateAverageDifficulty(data) {
-    let totalDifficulty = data.reduce(
-        (total, item) => total + item.difficulty,
-        0
-    );
-    return totalDifficulty / data.length;
-}
-
-module.exports = { calculateAverageDifficulty };
