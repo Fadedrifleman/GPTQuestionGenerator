c085b30aa12412f5b9054547948ae2e1bed60728
Shivam Uttam <102919037+Fadedrifleman@users.noreply.github.com>
2023-12-19 12:50:26 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
     generateQuestions,
     generateExplanation,
+    chatCompletion,
 } = require("../controllers/controllers");
 
 router.post("/generate", generateQuestions);
 router.post("/explain", generateExplanation);
+router.post("/completion", chatCompletion);
 
 module.exports = router;
